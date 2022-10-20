81d2ae06ba76f035617ef86d314c8b258370c762
marinetblom <u20477211@tuks.co.za>
2022-10-20 12:03:29 +0200

    Added more functionality

--- main.js
+++ main.js
@@ -27,11 +27,13 @@ const db = getDatabase(app);
 
 //Load a map to leaflet
 var map = L.map("map").setView([-25.754544, 28.231482], 14);
+map.setMaxBounds(map.getBounds());
 
 //Baselayer
 var tiles = L.tileLayer(
   "https://api.mapbox.com/styles/v1/marinetblom/cl9bohhha000w15r1zmza912y/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFyaW5ldGJsb20iLCJhIjoiY2w3NjJmMHdlMGZscjNudDhjNHFraDB2dyJ9.unx4-Uni0RveKtgu2YH_qA",
   {
+    minZoom: 15,
     maxZoom: 20,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -65,7 +67,7 @@ var green = L.icon({
   popupAnchor: [0, -30],
 });
 
-// Styling icons function
+// Styling icons with a function
 function myIcon(distance) {
   if (distance == 1) {
     return brown;
@@ -96,7 +98,6 @@ L.control.watermark = function (opts) {
 L.control.watermark({ position: "bottomleft" }).addTo(map);
 
 //Load points to map
-var marker = null;
 var NSFASYes = [];
 var NSFASNo = [];
 var All = [];
@@ -107,29 +108,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = snapshot.val();
     if (data) {
       data.forEach((res) => {
+        function marker() {
+          var marker = null;
+          marker = L.marker();
+          marker.setLatLng([res.coordinates[1], res.coordinates[0]]);
+          marker.setIcon(myIcon(res.Distance_to_Main));
+          const popupContent = `
+                  Name: ${res.Name} <br>
+                  Average Annual Price: R${res.Avg_Price}`;
+          marker.bindPopup(popupContent);
+          return marker;
+        }
         if (res.NSFAS_Acc == "Yes") {
-          marker = L.marker();
-          marker.setLatLng([res.coordinates[1], res.coordinates[0]]);
-          marker.setIcon(myIcon(res.Distance_to_Main));
-          const popupContent = `
-          Name: ${res.Name} <br>
-          Average Annual Price: R${res.Avg_Price}`;
-          marker.bindPopup(popupContent);
-          NSFASYes.push(marker);
+          NSFASYes.push(marker());
         } else if (res.NSFAS_Acc == "No") {
-          marker = L.marker();
-          marker.setLatLng([res.coordinates[1], res.coordinates[0]]);
-          marker.setIcon(myIcon(res.Distance_to_Main));
-          const popupContent = `
-          Name: ${res.Name} <br>
-          Average Annual Price: R${res.Avg_Price}`;
-          marker.bindPopup(popupContent);
-          NSFASNo.push(marker);
+          NSFASNo.push(marker());
         }
       });
 
-      let NSFASYesLayer = L.featureGroup(NSFASYes).addTo(map);
-      let NSFASNoLayer = L.featureGroup(NSFASNo).addTo(map);
+      // Create feature groups to Filter data
+      let NSFASYesLayer = L.featureGroup(NSFASYes);
+      let NSFASNoLayer = L.featureGroup(NSFASNo);
       All = NSFASYes.concat(NSFASNo);
       let AllLayer = L.featureGroup(All).addTo(map);
 
@@ -140,29 +139,49 @@ document.addEventListener("DOMContentLoaded", () => {
       };
       var overlayMaps = {
         All: AllLayer,
-        NSFASAcc: NSFASYesLayer,
-        NotAcc: NSFASNoLayer,
       };
 
       var layerControl = L.control
         .layers(baseMaps, overlayMaps, { collapsed: false })
         .addTo(map);
+
+      // The JavaScript below is new
+      $("#yes").click(function () {
+        map.addLayer(NSFASYesLayer);
+        map.removeLayer(NSFASNoLayer);
+      });
+      $("#no").click(function () {
+        map.addLayer(NSFASNoLayer);
+        map.removeLayer(NSFASYesLayer);
+      });
+      $("#all").click(function () {
+        map.addLayer(NSFASYesLayer);
+        map.addLayer(NSFASNoLayer);
+      });
     }
   });
 });
 
+//Search
+var controlSearch = new L.Control.Search({
+  position: "topleft",
+  layer: All,
+  initial: false,
+  zoom: 12,
+  marker: false,
+});
+
+map.addControl(controlSearch);
+
 /*Legend */
 var legend = L.control({ position: "bottomleft" });
-
 legend.onAdd = function (map) {
   var div = L.DomUtil.create("div", "legend");
   div.innerHTML += "<h4>Distance to Campus</h4>";
   div.innerHTML +=
-    '<i class="icon" style="background-image: url(brown.png);background-repeat: no-repeat;"></i><span>More than 1 km</span><br>';
+    '<i class="icon" style="background-image: url(brown.png);background-repeat: no-repeat;"></i><span>Greater than 1 km</span><br>';
   div.innerHTML +=
     '<i class="icon" style="background-image: url(green.png);background-repeat: no-repeat;"></i><span>Less than 1 km</span><br>';
-
   return div;
 };
-
 legend.addTo(map);
